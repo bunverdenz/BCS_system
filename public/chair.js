caa10db78c57e1e1c23e3237f09574316a61918d
BUNVERDENZ <bunverdenz@gmail.com>
2018-06-03 23:09:55 +0900

    no login

--- public/chair.js
+++ public/chair.js
@@ -27,58 +27,6 @@ $('.menu .browse')
     }
   })
 ;
-document.getElementById("login").onclick = function(){
-    console.log("Salem")
-    $('.ui.login.modal')
-    .modal('show')
-  ;
-  }
-  
-  $('.ui.login.form')
-    .form({
-      fields: {
-       
-        username: {
-          identifier: 'username',
-          rules: [
-            {
-              type   : 'empty',
-              prompt : 'Please enter a username'
-            }
-          ]
-        },
-        password: {
-          identifier: 'password',
-          rules: [
-            {
-              type   : 'empty',
-              prompt : 'Please enter a password'
-          
-            }
-          ]
-        },
-      }
-    });
-  
-  $('.ui.red.login.cancel.basic.button').click(function(){
-  console.log('click');
-  $('.ui.login.modal.form').modal('hide'); 
-  }),
-                                               
-   
-   $('.ui.green.login.submit.basic.button').click(function(){
-   console.log('click');
-   if( $('.ui.login.form').form('is valid')) {
-   console.log('valid');
-   $('.ui.login.modal').modal('hide'); 
-    
-    }
-       });
-  
-  document.getElementById("signUp").onclick = function(){
-        $('.ui.signup.modal')
-        .modal('show');
-  }
 
 var myChair = document.getElementById("myChair");
 
