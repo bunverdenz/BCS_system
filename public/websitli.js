005386c723e5ced776a0d0c9bc4c681c28c567ea
Shynar Torekhan <shynartorekhan@gmail.com>
2018-06-04 01:37:35 +0900

    buttons fully unctionall

--- public/websitli.js
+++ public/websitli.js
@@ -30,3 +30,69 @@ document.getElementById("b110").onclick = function(){
         window.location = "http://localhost:8080/ticketPurchase";
     });
 }
+document.getElementById("b1123").onclick = function(){
+  console.log("ENTERS")
+  $.post("http://localhost:8080/websloggedin",{movie_name:"Deadpool 2" ,time: "12:30"}).then(function(data) {
+        window.location = "http://localhost:8080/ticketPurchase";
+    });
+}
+
+
+document.getElementById("b215").onclick = function(){
+  console.log("ENTERS")
+  $.post("http://localhost:8080/websloggedin",{movie_name:"Avengers: Infinity War" ,time: "15:00"}).then(function(data) {
+        window.location = "http://localhost:8080/ticketPurchase";
+    });
+}
+document.getElementById("b2173").onclick = function(){
+  console.log("ENTERS")
+  $.post("http://localhost:8080/websloggedin",{movie_name:"Avengers: Infinity War" ,time: "17:30"}).then(function(data) {
+        window.location = "http://localhost:8080/ticketPurchase";
+    });
+}
+
+
+document.getElementById("b320").onclick = function(){
+  console.log("ENTERS")
+  $.post("http://localhost:8080/websloggedin",{movie_name:"Black Panther" ,time: "20:00"}).then(function(data) {
+        window.location = "http://localhost:8080/ticketPurchase";
+    });
+}
+document.getElementById("b39").onclick = function(){
+  console.log("ENTERS")
+  $.post("http://localhost:8080/websloggedin",{movie_name:"Black Panther" ,time: "09:00"}).then(function(data) {
+        window.location = "http://localhost:8080/ticketPurchase";
+    });
+}
+
+
+document.getElementById("b4113").onclick = function(){
+  console.log("ENTERS")
+  $.post("http://localhost:8080/websloggedin",{movie_name:"The Greatest Showman" ,time: "11:30"}).then(function(data) {
+        window.location = "http://localhost:8080/ticketPurchase";
+    });
+}
+document.getElementById("b412").onclick = function(){
+  console.log("ENTERS")
+  $.post("http://localhost:8080/websloggedin",{movie_name:"The Greatest Showman" ,time: "14:00"}).then(function(data) {
+        window.location = "http://localhost:8080/ticketPurchase";
+    });
+}
+
+
+document.getElementById("b5163").onclick = function(){
+  console.log("ENTERS")
+  $.post("http://localhost:8080/websloggedin",{movie_name:"Peter Rabbit" ,time: "16:30"}).then(function(data) {
+        window.location = "http://localhost:8080/ticketPurchase";
+    });
+}
+document.getElementById("b5193").onclick = function(){
+  console.log("ENTERS")
+  $.post("http://localhost:8080/websloggedin",{movie_name:"Peter Rabbit" ,time: "19:30"}).then(function(data) {
+        window.location = "http://localhost:8080/ticketPurchase";
+    });
+}
+
+// $('table').on('click', '.ui.positive.basic.small.button', function() {
+//     $(this).closest('tr').cells[0]
+//   });
